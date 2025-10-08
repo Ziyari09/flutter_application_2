01a5e634319a4578e61be7472ea1817ed780de32
Ziyari09 <algeming8@gmail.com>
2025-10-08 13:45:06 +0700

    comit 1

---
+++ bin/Dart Dasar/komentar.dart
@@ -0,0 +1,17 @@
+// Komentar Satu baris 
+void main(){
+
+  var name = 'Al Kautsar Diprajaya';
+  var kelas = "Teknik Infomatika";
+  int umur = 21;
+/** ini adalah komentar lebih dari satu baris
+ * 
+ *   print(name);
+  print(kelas);
+  print("Umur Aku "); print(umur);
+ */
+
+
+
+
+}
